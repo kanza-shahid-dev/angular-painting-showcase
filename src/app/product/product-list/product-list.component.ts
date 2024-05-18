f9b38fcd5b4c1486633d311c6a755a4f221d9679
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-19 01:24:13 +0500

    sorting feature

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -11,6 +11,7 @@ import { CartService } from '../../cart/cart.service';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   filteredProducts: Product[] = [];
+  sortOrder: string = '';
 
   constructor(
     private productService: ProductService,
@@ -36,5 +37,16 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.products.filter((product) =>
       product.name.toLowerCase().includes(searchTerm)
     );
+    this.sortProducts(this.sortOrder);
+  }
+
+  sortProducts(sortValue: string) {
+    this.sortOrder = sortValue;
+    console.log('sortProducts', this.sortOrder);
+    if (this.sortOrder === 'priceLowHigh') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (this.sortOrder === 'priceHighLow') {
+      this.filteredProducts.sort((a, b) => b.price - a.price);
+    }
   }
 }
